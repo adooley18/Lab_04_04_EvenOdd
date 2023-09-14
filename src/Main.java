5963fc62e6dba00e613ee2c61d7daf3eb3fdaddb
ajdooley18 <ajdooley16@gmail.com>
2023-09-14 17:45:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0)
+        {
+            System.out.println("Your number is even!");
+        }
+        else
+        {
+            System.out.println("Your number is odd!");
+        }
+    }
+}
